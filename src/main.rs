ec8211e90b2959c977a0b8d01a7a4ad151a35e3c
Ankit Choubey <142090825+AnkitCh0ubey@users.noreply.github.com>
2024-10-21 03:22:37 +0530

    Initial Push: XML parser and the docs.gl directory

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+use std::fs::{File,self};
+use xml::reader::{EventReader, XmlEvent };
+use std::path::Path;
+
+fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String>{ 
+   let file = File::open(file_path)?;
+   
+   let er = EventReader::new(file);
+   let mut content = String::new();
+   
+   for event in er.into_iter(){
+      if let XmlEvent::Characters(text) = event.expect("Will wrap the xml error and the io error together"){
+         content.push_str(&text);
+      }
+   }
+   Ok(content)
+}
+
+fn main() -> io::Result<()>{
+   let dir_path = "docs.gl/gl4";
+   let dir = fs::read_dir(dir_path)?;
+   
+   for file in dir{
+      let file_path = file?.path();
+      let content = read_entire_xml_file(&file_path)?;
+   
+      println!("{file_path:?} => {size}", size = content.len());
+   }
+   Ok(())
+}
